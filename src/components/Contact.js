04fa47905602ca2b4671ca9e83ae3e13ae91fee3
Sara Valencia <sara.valencia.m@gmail.com>
2021-07-26 09:24:34 +0200

    contact section working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,12 +7,49 @@
   import InputContact from './atoms/InputContact'
   import TextArea from './atoms/TextArea'
   import '../App.css'
+  import emailjs from 'emailjs-com';
+  import styled from 'styled-components'
   
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 30em;
+    margin-top: 3em;
+    padding-left: 2em;
 
+    label{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: 500;
+        font-size: 1.3em;
+        margin-top: 1em;
+    }
+    input {
+        height: 2em;
+        border: none;
+    }
+    textarea {
+     width: 100%;
+     height: 20em;
+     border: none;
+ }
+  `;
 
 
 
   function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_a7eumdd', 'template_al7dqei', e.target, 'user_dv0vo2Dt1rpR8kzyk2cXD')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
+
     return (
     
         <ContainerContact>
@@ -26,14 +63,15 @@ Udfyld kontaktformularen til højre eller send  en mail <br/> på redefine@outlo
                 {/* <P stylingForContact>Based in Denmark.<br/> Working remote and worldwide.</P> */}
             </ContactDiv>
             <ContactDiv stylingForContact>
-                <P stylingForContactName>Name*</P>
-                <InputContact/>
-                <P stylingForContactEmail>Email*</P>
-                <InputContact stylingForContactEmail/>
-                <P stylingForContactMessage>Message*</P>
-                <TextArea></TextArea>
-                <button className="buttonContact">Send</button>
-                
+                <Form onSubmit={sendEmail}>
+                <label>Name</label>
+      <input type="text" name="name" placeholder='Enter your name' id='name'/>
+      <label>Email</label>
+      <input type="email" name="email"  placeholder='Enter your email' id='email' />
+      <label  >Message</label>
+      <textarea name="message" placeholder='Enter your message' />
+      <button className="buttonContact">Send</button>
+                </Form >
                 
             </ContactDiv>
 
